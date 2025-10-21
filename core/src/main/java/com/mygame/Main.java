1b24df0616b6c23fe928587c3e237b1572d5d276
Oleksandr <sasha291108@gmail.com>
2025-10-21 11:06:43 +0200

    npcManager feature

--- core/src/main/java/com/mygame/Main.java
+++ core/src/main/java/com/mygame/Main.java
@@ -10,16 +10,16 @@ import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.utils.viewport.FitViewport;
 import com.badlogic.gdx.utils.viewport.Viewport;
-import java.util.ArrayList;
 
 
 public class Main extends ApplicationAdapter {
 
     // === Основні ігрові об'єкти ===
     private Player player;
-    private ArrayList<NPC> npcs = new ArrayList<>();
+
     private World world;
     private UIManager uiManager;
+    private NpcManager npcManager;
 
     // === Рендеринг та графіка ===
     private SpriteBatch batch;
@@ -29,14 +29,6 @@ public class Main extends ApplicationAdapter {
 
     // Текстури
     private Texture textureZoe;
-    private Texture textureRyzhyi;
-    private Texture textureDenys;
-    private Texture textureIgo;
-    private Texture textureIgo2;
-    private Texture textureBaryga;
-    private Texture textureChikita;
-    private Texture texturePolice;
-
 
     // === Константи світу ===
     private static final int WORLD_WIDTH = 4000;
@@ -49,13 +41,6 @@ public class Main extends ApplicationAdapter {
         // === Ініціалізація базових систем ===
         batch = new SpriteBatch();
         textureZoe = new Texture("zoe.png");
-        textureRyzhyi = new Texture("ryzhyi.png");
-        textureDenys = new Texture("denys.png");
-        textureIgo = new Texture("igo.png");
-        textureIgo2 = new Texture("igo2.png");
-        textureBaryga = new Texture("baryga.png");
-        textureChikita = new Texture("chikita.png");
-        texturePolice = new Texture("police.png");
 
         font = new BitmapFont();
         font.getData().setScale(2.5f);
@@ -68,78 +53,7 @@ public class Main extends ApplicationAdapter {
         // === Гравець та UI Manager ===
         player = new Player(500, 100, 100, 200, 200, textureZoe, world);
         uiManager = new UIManager(player);
-
-        // === NPC ===
-        NPC igo = new NPC("Igo",100, 100, 500, 300, textureIgo, world, 1, 0, 3f, 0f,0,
-            new String[]{"Hallo Bruder!", "Gib kosyak"});
-        npcs.add(igo);
-
-        igo.setAction(() -> {
-            if (igo.getDialogueCount() == 1)
-                if(player.getInventory().removeItem("kosyak", 1)) {
-                    player.getInventory().addItem("Vape", 1);
-                    uiManager.showInfoMessage("You got 1 Vape", 1.5f);
-                    uiManager.updateQuestMessage("");
-                    igo.nextDialogueCount();
-                    igo.setTexts(new String[]{"Danke Bruder!"});
-                    igo.setTexture(textureIgo2);
-                } else {
-                    uiManager.showInfoMessage("Not enough kosyak", 1.5f);
-                    uiManager.updateQuestMessage("Get some kosyak for igo");
-                }
-        });
-
-        NPC ryzhyi = new NPC("Ryzhyi",100, 100, 1100, 500, textureRyzhyi, world, 0, 1, 1f, 2f,200,
-            new String[]{"Please take 10 euro but fuck off"});
-        npcs.add(ryzhyi);
-
-        ryzhyi.setAction(() -> {
-            if (ryzhyi.getDialogueCount() == 1) {
-                player.getInventory().addItem("money", 10);
-                uiManager.showInfoMessage("You got 10 euro",1.5f);
-                ryzhyi.nextDialogueCount();
-                ryzhyi.setTexts(new String[]{"I gave 10 euro why do I still see you "});
-            }
-        });
-
-        NPC denys = new NPC("Denys",100, 100, 700, 700, textureDenys, world, 1, 1,2f, 1f, 100,
-            new String[]{"Hello Popa!!!", "I'm not in mood to talk"});
-        npcs.add(denys);
-
-        NPC baryga = new NPC("Baryga",100, 100, 1000, 200, textureBaryga, world, 0, 1, 3f, 0f, 0,
-            new String[]{"Bruder was brauchst du?", "Grass 10 Euro"});
-        npcs.add(baryga);
-
-        baryga.setAction(() -> {
-            if (player.getInventory().removeItem("money",10)) {
-                player.getInventory().addItem("grass", 1);
-                uiManager.showInfoMessage("You got 1g grass for 10 euro", 1.5f);
-            } else {
-                uiManager.showInfoMessage("Not enough money", 1.5f);
-            }
-        });
-
-        NPC chikita = new NPC("Chikita",100, 100, 1575, 200, textureChikita, world, 0, 1, 3f, 0f, 0,
-            new String[]{"Gib grass du bekommen kosyak"});
-        npcs.add(chikita);
-
-        chikita.setAction(() -> {
-            if (player.getInventory().removeItem("grass",1)) {
-                player.getInventory().addItem("kosyak", 1);
-                uiManager.showInfoMessage("You got 1 kosyak", 1.5f);
-            } else {
-                uiManager.showInfoMessage("Not enough grass", 1.5f);
-            }
-        });
-
-        NPC police = new NPC("Police",120, 120, 575, 350, texturePolice, world, 1, 0, 3f, 6f, 75,
-            new String[]{"Polizeikontrolle, haben Sie Grass?"});
-        npcs.add(police);
-
-        police.setAction(() -> {
-            player.getInventory().removeItem("grass", 1);
-            player.getInventory().removeItem("kosyak", 1);
-        });
+        npcManager = new NpcManager(batch, player,world,uiManager,font);
     }
 
     @Override
@@ -148,8 +62,8 @@ public class Main extends ApplicationAdapter {
 
         // === Оновлення ігрової логіки ===
         player.update(delta);
-        for (NPC npc : npcs) npc.update(delta);
-        uiManager.update(delta, player, npcs);
+
+        uiManager.update(delta, player, npcManager.getNpcs());
 
         // === Камера слідкує за гравцем ===
         float targetX = player.x + player.width / 2f;
@@ -169,16 +83,13 @@ public class Main extends ApplicationAdapter {
         batch.begin();
         world.draw(batch);
         player.draw(batch);
-        for (NPC npc : npcs) {
-            npc.draw(batch);
-            if (npc.isPlayerNear(player)) {
-                font.draw(batch, "Press E / ACT to interact", npc.x - 100, npc.y + npc.height + 40);
-            }
-        }
+
+        npcManager.render();
         batch.end();
 
         // --- UI ---
         uiManager.render();
+
     }
 
     @Override
@@ -191,15 +102,10 @@ public class Main extends ApplicationAdapter {
     public void dispose() {
         // === Очищення пам’яті ===
         textureZoe.dispose();
-        textureRyzhyi.dispose();
-        textureDenys.dispose();
-        textureIgo.dispose();
-        textureBaryga.dispose();
-        textureChikita.dispose();
-        texturePolice.dispose();
         batch.dispose();
         font.dispose();
         world.dispose();
         uiManager.dispose();
+        npcManager.dispose();
     }
 }
---
+++ core/src/main/java/com/mygame/NpcManager.java
@@ -0,0 +1,127 @@
+package com.mygame;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
+import java.util.ArrayList;
+
+public class NpcManager {
+    private ArrayList<NPC> npcs = new ArrayList<>();
+    private Texture textureRyzhyi;
+    private Texture textureDenys;
+    private Texture textureIgo;
+    private Texture textureIgo2;
+    private Texture textureBaryga;
+    private Texture textureChikita;
+    private Texture texturePolice;
+
+    private Player player;
+    private SpriteBatch batch;
+    private BitmapFont font;
+
+    public NpcManager(SpriteBatch batch, Player player, World world, UIManager uiManager, BitmapFont font) {
+        this.batch = batch;
+        this.player = player;
+        this.font = font;
+        textureRyzhyi = new Texture("ryzhyi.png");
+        textureDenys = new Texture("denys.png");
+        textureIgo = new Texture("igo.png");
+        textureIgo2 = new Texture("igo2.png");
+        textureBaryga = new Texture("baryga.png");
+        textureChikita = new Texture("chikita.png");
+        texturePolice = new Texture("police.png");
+
+        NPC igo = new NPC("Igo",100, 100, 500, 300, textureIgo, world, 1, 0, 3f, 0f,0,
+            new String[]{"Hallo Bruder!", "Gib kosyak"});
+        npcs.add(igo);
+
+        igo.setAction(() -> {
+            if (igo.getDialogueCount() == 1)
+                if(player.getInventory().removeItem("kosyak", 1)) {
+                    player.getInventory().addItem("Vape", 1);
+                    uiManager.showInfoMessage("You got 1 Vape", 1.5f);
+                    uiManager.updateQuestMessage("");
+                    igo.nextDialogueCount();
+                    igo.setTexts(new String[]{"Danke Bruder!"});
+                    igo.setTexture(textureIgo2);
+                } else {
+                    uiManager.showInfoMessage("Not enough kosyak", 1.5f);
+                    uiManager.updateQuestMessage("Get some kosyak for igo");
+                }
+        });
+
+        NPC ryzhyi = new NPC("Ryzhyi",100, 100, 1100, 500, textureRyzhyi, world, 0, 1, 1f, 2f,200,
+            new String[]{"Please take 10 euro but fuck off"});
+        npcs.add(ryzhyi);
+
+        ryzhyi.setAction(() -> {
+            if (ryzhyi.getDialogueCount() == 1) {
+                player.getInventory().addItem("money", 10);
+                uiManager.showInfoMessage("You got 10 euro",1.5f);
+                ryzhyi.nextDialogueCount();
+                ryzhyi.setTexts(new String[]{"I gave 10 euro why do I still see you "});
+            }
+        });
+
+        NPC denys = new NPC("Denys",100, 100, 700, 700, textureDenys, world, 1, 1,2f, 1f, 100,
+            new String[]{"Hello Popa!!!", "I'm not in mood to talk"});
+        npcs.add(denys);
+
+        NPC baryga = new NPC("Baryga",100, 100, 1000, 200, textureBaryga, world, 0, 1, 3f, 0f, 0,
+            new String[]{"Bruder was brauchst du?", "Grass 10 Euro"});
+        npcs.add(baryga);
+
+        baryga.setAction(() -> {
+            if (player.getInventory().removeItem("money",10)) {
+                player.getInventory().addItem("grass", 1);
+                uiManager.showInfoMessage("You got 1g grass for 10 euro", 1.5f);
+            } else {
+                uiManager.showInfoMessage("Not enough money", 1.5f);
+            }
+        });
+
+        NPC chikita = new NPC("Chikita",100, 100, 1575, 200, textureChikita, world, 0, 1, 3f, 0f, 0,
+            new String[]{"Gib grass du bekommen kosyak"});
+        npcs.add(chikita);
+
+        chikita.setAction(() -> {
+            if (player.getInventory().removeItem("grass",1)) {
+                player.getInventory().addItem("kosyak", 1);
+                uiManager.showInfoMessage("You got 1 kosyak", 1.5f);
+            } else {
+                uiManager.showInfoMessage("Not enough grass", 1.5f);
+            }
+        });
+
+        NPC police = new NPC("Police",120, 120, 575, 350, texturePolice, world, 1, 0, 3f, 6f, 75,
+            new String[]{"Polizeikontrolle, haben Sie Grass?"});
+        npcs.add(police);
+
+        police.setAction(() -> {
+            player.getInventory().removeItem("grass", 1);
+            player.getInventory().removeItem("kosyak", 1);
+        });
+    }
+
+    public void render(){
+        float delta = Gdx.graphics.getDeltaTime();
+        for (NPC npc : npcs) npc.update(delta);
+        for (NPC npc : npcs) {
+            npc.draw(batch);
+            if (npc.isPlayerNear(player)) {
+                font.draw(batch, "Press E / ACT to interact", npc.x - 100, npc.y + npc.height + 40);
+            }
+        }
+    }
+    public void dispose(){
+        textureRyzhyi.dispose();
+        textureDenys.dispose();
+        textureIgo.dispose();
+        textureBaryga.dispose();
+        textureChikita.dispose();
+        texturePolice.dispose();
+    }
+    public ArrayList<NPC> getNpcs() {return npcs;}
+}
